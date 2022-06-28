0d1a3b807821109be133c8d7b3f59b4d6e3edeae
victor kenneth <kennethvic07@gmail.com>
2022-06-28 15:05:07 +0100

    created model and added to app

--- instagram/models.py
+++ instagram/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.models import User
 
-# Create your models here.
+class Post(models.Model):
+    user = Foreignkey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='image')
+    description = models.TextField()
+    like = models.ManyToManyField(User,default=0, blank=True, related_name='post_like')
+    follow = models.ManyToManyField(User, dafault=0, blank=True, related_name='post_follow')
+
+    def __str__(self):
+        return self.user
